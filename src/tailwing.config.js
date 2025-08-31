c1938982ca9f827b5d3071ac2ef9ea14bc09c041
manojvishalkijo <manojvishal125@gmail.com>
2025-08-31 12:02:48 +0530

    first commit

--- src/tailwing.config.js
+++ src/tailwing.config.js
@@ -2,16 +2,10 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {
-      fontSize: {
-        '8xl': '6rem',   // 96px
-        '9xl': '8rem',   // 128px
-        '10xl': '10rem', // 160px (if you want really big)
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
