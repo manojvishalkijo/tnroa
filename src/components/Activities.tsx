4dd2467ad34ca36bf41541d0b1760b8489c930b3
manojvishalkijo <manojvishal125@gmail.com>
2025-08-31 12:54:47 +0530

    first commit

--- src/components/Activities.tsx
+++ src/components/Activities.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import '../styles/activities.css';
+import '../styles/Activities.css';
 import UtilityBar from "./navigation/UtilityBar";
 import { Link } from "react-router-dom";
 
